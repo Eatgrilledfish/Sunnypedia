ebd27833ef758057f2c751766a04b36576bce06c
Jianqiao <47028195+Eatgrilledfish@users.noreply.github.com>
2024-03-05 04:51:15 +1100

    first try

--- Front/src/App.vue
+++ Front/src/App.vue
@@ -1,47 +1,64 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <el-container class="home-container">
+    <el-header>
+      <h1>SunnyPedia</h1>
+      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
+        <el-menu-item index="1">UV Index</el-menu-item>
+        <el-menu-item index="2">Clothing</el-menu-item>
+        <el-menu-item index="3">Sunscreen Calculator</el-menu-item>
+        <el-menu-item index="4">UVAware</el-menu-item>
+        <el-menu-item index="5">Notifications</el-menu-item>
+      </el-menu>
+    </el-header>
+    <el-main>
+      <div class="content">
+        <h2>Empowering Families, Protecting Futures:</h2>
+        <p>Your Guide to Sun Safety</p>
+      </div>
+      <!-- 忽略图片的部分 -->
+      <div>
+        <p>Content or placeholders can go here.</p>
+      </div>
+    </el-main>
+  </el-container>
 </template>
 
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      activeIndex: '1'
+    };
+  }
+};
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.home-container {
+  text-align: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.el-header {
+  background-color: #333;
+  color: white;
+  padding: 20px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.el-menu-demo {
+  background-color: transparent;
+  border: none;
 }
+
+.content h2,
+.content p {
+  color: #333;
+}
+
+.el-main {
+  background-color: #f5f5f5; /* Changed to a plain color for background */
+  padding: 60px;
+}
+
+/* ...additional styles... */
 </style>
